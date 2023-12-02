4a06de331f6cfb8786286a65e2cdc34393e7e02c
KKN18 <kimkinam1118@kaist.ac.kr>
2023-12-02 05:15:02 +0000

    feat: create files

--- dataset_preprocessing/ffhq/preprocess_in_the_wild.py
+++ dataset_preprocessing/ffhq/preprocess_in_the_wild.py
@@ -29,16 +29,16 @@ os.system(command)
 os.chdir('..')
 
 # crop out the input image
-command = "python crop_images_in_the_wild.py --indir=" + args.indir
-print(command)
-os.system(command)
+# command = "python crop_images_in_the_wild.py --indir=" + args.indir
+# print(command)
+# os.system(command)
 
 # convert the pose to our format
-command = f"python 3dface2idr_mat.py --in_root Deep3DFaceRecon_pytorch/checkpoints/pretrained/results/{out_folder}/epoch_20_000000 --out_path {os.path.join(args.indir, 'crop', 'cameras.json')}"
+command = f"python 3dface2idr_mat.py --in_root Deep3DFaceRecon_pytorch/checkpoints/pretrained/results/{out_folder}/epoch_20_000000 --out_path {os.path.join(args.indir, 'cameras.json')}"
 print(command)
 os.system(command)
 
 # additional correction to match the submission version
-command = f"python preprocess_face_cameras.py --source {os.path.join(args.indir, 'crop')} --dest {out_folder} --mode orig"
+command = f"python preprocess_face_cameras.py --source {os.path.join(args.indir)} --dest {out_folder} --mode orig"
 print(command)
 os.system(command)
